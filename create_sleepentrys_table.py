df1c2387c8eed7dd78dd46d4b2fc2df8de6f7909
yen nguyen <hoangyenmailbox@gmail.com>
2022-12-03 10:55:54 +0000

    fix a little the table in db

--- create_sleepentrys_table.py
+++ create_sleepentrys_table.py
@@ -11,13 +11,11 @@ mycursor = mydb.cursor()
 
 mycursor.execute("""
 CREATE TABLE Sleepentry (
-  sleep_id int,
   user_id int,
   sleepdate DATE,
   sleeptime DATETIME,
   wakeuptime DATETIME,
   totalsleep TIME,
-  FOREIGN KEY(user_id) REFERENCES Users(user_id),
-  PRIMARY KEY(sleep_id)
+  FOREIGN KEY(user_id) REFERENCES Users(user_id)
 )
 """)
